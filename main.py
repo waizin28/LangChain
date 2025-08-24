f7a1197ff2cefb7c20fdf426c69fc69901862282
waizin28 <waizinlinn.08@gmail.com>
2025-08-23 19:49:24 -0500

    environment setup

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def main():
+    print("Hello from langchain-course!")
+    print(os.environ.get("OPENAI_API_KEY"))
+
+if __name__ == "__main__":
+    main()
